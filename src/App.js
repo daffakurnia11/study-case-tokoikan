437d660d99defecd3bd420170f222bb7983bf57a
Daffa Kurnia Fatah <daffa.kurnia@lndata.com>
2023-05-17 13:42:14 +0700

    feat: :sparkles: Create base-component for each part of landing page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
+import Contact from "./Components/Contact";
+import Features from "./Components/Features";
+import Footer from "./Components/Footer";
+import Header from "./Components/Header";
+import Navbar from "./Components/Navbar";
+import Privilege from "./Components/Privilege";
+import Testimonials from "./Components/Testimonials";
+import Tracking from "./Components/Tracking";
+
 function App() {
   return (
-    <div className="App">
-      <>Test</>
-    </div>
+    <>
+      <Navbar />
+      <Header />
+      <Tracking />
+      <Privilege />
+      <Features />
+      <Testimonials />
+      <Footer />
+      <Contact />
+    </>
   );
 }
 
