d68cbb4865dcef3dab5b07d958e65741ad61691d
Daffa Kurnia Fatah <daffa.kurnia@lndata.com>
2023-05-17 17:32:37 +0700

    feat(testimonial): :sparkles: Create header of testimonial

--- src/Components/Testimonials.js
+++ src/Components/Testimonials.js
@@ -1,5 +1,16 @@
 import React from "react";
+import { Container } from "react-bootstrap";
 
 export default function Testimonials() {
-  return <div>Testimonials</div>;
+  return (
+    <section className="testimoni">
+      <Container>
+        <h2 className="testimoni-title text-center">Testimonial</h2>
+        <p className="testimoni-desc text-center">
+          A Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text
+        </p>
+      </Container>
+    </section>
+  );
 }
