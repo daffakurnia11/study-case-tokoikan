9a6394065df452e12de087849d1347e6eedb7768
Daffa Kurnia Fatah <daffa.kurnia@lndata.com>
2023-05-17 16:15:53 +0700

    feat(footer): :sparkles: Create responsive footer section

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,5 +1,50 @@
 import React from "react";
+import { Button, Col, Container, Row } from "react-bootstrap";
+import GooglePlayLogo from "../img/google-play-logo.svg";
+import GooglePlayLabel from "../img/google-play-label.svg";
+import AppStoreLogo from "../img/app-store-logo.svg";
+import AppStoreLabel from "../img/app-store-label.svg";
+import CourierImg from "../img/courier-img.png";
 
 export default function Footer() {
-  return <div>Footer</div>;
+  return (
+    <section className="footer">
+      <Container>
+        <Row>
+          <Col lg={6}>
+            <div
+              className="px-xxl-5"
+              style={{ paddingTop: 70, paddingBottom: 70 }}
+            >
+              <h2 className="footer-title text-center text-lg-start">
+                Available and Download Anytime!
+              </h2>
+              <p className="footer-desc text-center text-lg-start">
+                A Lorem Ipsum is simply dummy text of the printing and{" "}
+              </p>
+              <div className="d-flex my-sm-5 mt-3 mb-5 justify-content-center justify-content-lg-start flex-column flex-sm-row align-items-center">
+                <Button className="footer-button d-flex me-sm-4 mb-4 mb-sm-0">
+                  <img src={GooglePlayLogo} alt="google-play-logo.svg" />
+                  <img src={GooglePlayLabel} alt="google-play-label.svg" />
+                </Button>
+                <Button className="footer-button d-flex">
+                  <img src={AppStoreLogo} alt="app-store-logo.svg" />
+                  <img src={AppStoreLabel} alt="app-store-label.svg" />
+                </Button>
+              </div>
+            </div>
+          </Col>
+          <Col lg={6}>
+            <div className="footer-img">
+              <img
+                src={CourierImg}
+                className="courier-img"
+                alt="courier-img.png"
+              />
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  );
 }
